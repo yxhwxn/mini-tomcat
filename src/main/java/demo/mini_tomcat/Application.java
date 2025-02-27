39c93cf6dcf0882d928c9d9ac52f47bb32d74677
yxhwxn <officeyh9819@gmail.com>
2025-02-27 12:32:53 +0900

    tomcat 프로젝트 세팅, build dependency 추가

---
+++ src/main/java/demo/mini_tomcat/Application.java
@@ -0,0 +1,16 @@
+package demo.mini_tomcat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        log.info("web server start.");
+        final var tomcat = new Tomcat();
+        tomcat.start();
+    }
+
+}
